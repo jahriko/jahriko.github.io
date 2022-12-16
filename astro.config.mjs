e543ded40f872aa42581491899215be89f1e301f
Jericho Opsima <jerichoopsima22@gmail.com>
2022-12-16 20:17:15 +0800

    astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://jahriko.github.io',
 	// Enable React to support React JSX components.
 	integrations: [
 		react(),
